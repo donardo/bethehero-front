d7ae5f8c4c0e2f6b78817fd097a1ba86ea1dac28
Douglas Donardo <douglasddd@gmail.com>
2020-03-30 12:07:41 -0300

    Fontend da Aplicação

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React  from 'react';
+//import Header from './Header'
+import './global.css';
+//import Logon from './pages/Logon';
+import Routes from './routes'
+
+
+
+function App() {
+  //const [counter, setCounter] = useState(0);
+  // Array [valor, funcaoDeAtualizao]
+  /* function increment() {
+    //counter += 1;
+    setCounter(counter + 1);
+    console.log(counter);
+  } */
+
+  return (
+    <Routes />
+  );
+}
+
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+
+const api = axios.create({
+    //baseURL: 'http://localhost:3333',
+    baseURL: BASE_URL,
+})
+
+export default api;
